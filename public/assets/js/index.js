f0b56a5c0bbf95b5cd0a716f81e118c4b582b68a
Tom McLaughlin <mclaughlintomd@gmail.com>
2020-04-13 20:59:49 -0500

    added space for a carousel on index and a js file

---
+++ public/assets/js/index.js
@@ -0,0 +1,6 @@
+// js file for the index.html page
+// provides the changing src files for the carousel (currently)
+// maybe something more interesting later
+$(document).ready(function(){
+    $("#carouselBackground").append("<h1>Carousel goes here</h1>");
+});
