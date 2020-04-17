7234e48398d47ee287cbbc8512b762b1712bb256
Tom McLaughlin <mclaughlintomd@gmail.com>
2020-04-17 18:27:29 -0500

    added kitten pics in an array

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,8 @@
 // provides the changing src files for the carousel (currently)
 // maybe something more interesting later
 $(document).ready(function(){
+    // build array of kitten pics
+    var kittenPics = ["derp.jpg", "fours.jpg", "littlelady.png", "pairs.jpg", "sleepy.png"];
     $("#carouselBackground").append("<div id='innerCar' class='carousel slide'" +
         " data-ride='carousel' data-interval='5000' data-wrap='true'>" +
         "<ol class='carousel-indicators'>" +
@@ -14,23 +16,28 @@ $(document).ready(function(){
             
         "<div class='carousel-inner' role='listbox'>" +
         "<div class='item active'>" +
-        "<img class='img-responsive center-block myImage' src='assets/img/derp.jpg' alt='cuteone'>" +
-        "</div>" +
+        "<img class='img-responsive center-block myImage' src='assets/img/" +
+        kittenPics[0] +
+        "' alt='cuteone'></div>" +
 
         "<div class='item'>" +
-        "<img class='img-responsive center-block myImage' src='assets/img/fours.jpg' alt='cutetwo'>" +
-        "</div>" +
+        "<img class='img-responsive center-block myImage' src='assets/img/" +
+        kittenPics[1] +
+        "' alt='cutetwo'></div>" +
 
         "<div class='item'>" +
-        "<img class='img-responsive center-block myImage' src='assets/img/littlelady.png' alt='cutethree'>" +
-        "</div>" +
+        "<img class='img-responsive center-block myImage' src='assets/img/" +
+        kittenPics[2] +
+        "' alt='cutethree'></div>" +
 
         "<div class='item'>" +
-        "<img class='img-responsive center-block myImage' src='assets/img/pairs.jpg' alt='cutefour'>" +
-        "</div>" +
+        "<img class='img-responsive center-block myImage' src='assets/img/" +
+        kittenPics[3] +
+        "' alt='cutefour'></div>" +
 
         "<div class='item'>" +
-        "<img class='img-responsive center-block myImage' src='assets/img/sleepy.png' alt='cutefive'>" +
-        "</div></div></div>");
+        "<img class='img-responsive center-block myImage' src='assets/img/" +
+        kittenPics[4] +
+        "' alt='cutefive'></div></div></div>");
         $("#innerCar").carousel();
 });
