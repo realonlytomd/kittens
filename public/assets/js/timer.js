bbc0f5208990b08342545ebb8a7dc71c0863fab8
Tom McLaughlin <mclaughlintomd@gmail.com>
2020-07-06 12:29:26 -0500

    changed timer countdown to seconds and assoc display

--- public/assets/js/timer.js
+++ public/assets/js/timer.js
@@ -1,6 +1,7 @@
   // This file is just the timer function for feeding the kitten
   // testing to see if this can be called from the user.js file
   // and eventually, can keep the clock running between page changes
+  var displayStartCount;
 
   function feedKittenTimer() {
     // function to bring up the countdown clock to feed the kitten
@@ -19,7 +20,11 @@
     // check to see if the input value exists (or is empty or undefined)
     if (($("#startCount").val()))  {
       console.log("$('#startCount').val().trim() at beginning of click event: " + $("#startCount").val().trim());
-      startCount = $("#startCount").val().trim();
+      // timer should count down in seconds, because a user will be switching back
+      // and forth between pages. The startCount and myTimer variables will
+      // be reset. If under 1, a user could conceiveably never reach zero.
+      // Therefore, the initial requested timer should be multiplied by 60
+      startCount = $("#startCount").val().trim() * 60;
       console.log("after setting to .val().trim(), startCount: " + startCount);
       //reset form after retrieving input so input is undefined
       // for timer to be started from other pages with an undefined input.
@@ -38,21 +43,22 @@
   function timer() {
     //this span is in a fixed div so user sees it on page while the timer is running
     // if there is no timer, as in, the timer has run out, 
-    // or has never been set...THAT is what I need to test for
-    // so the timer banner doesn't show
-      $("span#timerDisplay").html(startCount);
-      localStorage.setItem("startCount", startCount);
-      if (startCount <= 1) {
-        $("span#timerLabel").text(" Minute Left");
-        } else {
-          $("span#timerLabel").text(" Minutes Left");
-        }
+    // or has never been set...timer() is not running and
+    // the timer banner doesn't show
+    // the timer is displayed in minutes for the user, hence the division by 60
+    // math.ceil rounds up to nearest integer, so user sees in full minutes.
+    $("span#timerDisplay").html(Math.ceil(startCount / 60));
+    localStorage.setItem("startCount", startCount);
+    if (startCount <= 60) {
+      $("span#timerLabel").text(" Minute Left");
+    } else {
+      $("span#timerLabel").text(" Minutes Left");
+    }
     $("#feedTimer").show();
-    // countdown startCount with a prefix decrement (because I'm doing it AFTER I use it??)
     startCount = startCount - 1;
     console.log("in function timer() after startCount - 1, startCount = " + startCount);
     // add play a sound as startCount reaches 0?
-    myTimer = setTimeout(function(){ timer() }, 60000);
+    myTimer = setTimeout(function(){ timer() }, 1000);
     //console.log("what is myTimer: " + myTimer);
     //console.log("typeof myTimer: " + typeof myTimer);
     localStorage.setItem("myTimer", myTimer);
