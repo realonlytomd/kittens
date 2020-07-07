de8773d0144e30a4c9c4fb31b1d636c587df7edd
Tom McLaughlin <mclaughlintomd@gmail.com>
2020-07-06 20:11:44 -0500

    deleted bad pic and updated index

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -13,13 +13,13 @@ $(document).ready(function(){
     // localStorage.setItem("currentUserLoggedIn", "false"); // all users are not logged in on index page
     // build array of kitten pics
     var kittenLandPics = ["alonewaterland.jpg", "angryishland.jpg", "angryland.jpg", 
-        "beautyland.jpg", "boxland.jpg", "browngrapland.jpg", "busterland.jpg", 
-        "chairland.jpg", "derpland.jpg", "djkittyland.jpg", "fattyland.jpg", 
+        "beautyland.jpg", "boxland.jpg", "browngrapland.jpg", "busterland.jpg",
+        "derpland.jpg", "djkittyland.jpg", "fattyland.jpg", 
         "fourwaterland.jpg", "groupland.jpg", "littleladyland.png", "momland.jpg", 
         "pairsland.jpg", "regalwaterland.jpg", "scrambleland.jpg", "screamland.png", 
         "sleepy2land.png", "threehugland.jpg", "threesland.jpg", "tubland.jpg", "washland.jpg"];
     var kittenPortPics = ["alonewaterport.jpg", "awport.jpg", "beansport.jpg", "beautyport.jpg", 
-        "browngrayport.jpg", "busterport.jpg", "carseatport.jpg", "chairport.jpg", "closeupport.jpg",
+        "browngrayport.jpg", "busterport.jpg", "carseatport.jpg", "closeupport.jpg",
         "couchport.jpg", "cribport.jpg", "curlyport.jpg", "eyelineport.jpg", "eyeport.jpg", "eyesport.jpg",
         "faroffport.jpg", "fluffyport.png", "foursport.jpg", "furport.jpg", "grayport.jpg", "littlelady2port.png",
         "pairsport.jpg", "purplethreeport.jpg", "sadport.jpg", "sleepyport.jpg", "strangleport.jpg",
@@ -27,59 +27,22 @@ $(document).ready(function(){
         "updownport.jpg", "whatport.jpg", "whiskersport.jpg", "whosport.jpg", "winnerport.jpg", 
         "xmasballport.jpg", "yellowcloseport.jpg", "busterkittenport.png"];
     var kittenIndex;
-    // $("#carouselBackground").append("<div id='innerCar' class='carousel slide'" +
-    //     " data-ride='carousel' data-interval='5000' data-wrap='true'>" +
-    //     "<ol class='carousel-indicators'>" +
-    //     "<li data-target='#innerCar' data-slide-to='0' class='active'></li>" +
-    //     "<li data-target='#innerCar' data-slide-to='1'></li>" +
-    //     "<li data-target='#innerCar' data-slide-to='2'></li>" +
-    //     "<li data-target='#innerCar' data-slide-to='3'></li>" +
-    //     "<li data-target='#innerCar' data-slide-to='4'></li>" +
-    //     "</ol>" +
-            
-    //     "<div class='carousel-inner' role='listbox'>" +
-    //     "<div class='item active'>" +
-    //     "<img class='img-responsive center-block myImage' src='assets/img/" +
-    //     kittenPics[0] +
-    //     "' alt='cuteone'></div>" +
-
-    //     "<div class='item'>" +
-    //     "<img class='img-responsive center-block myImage' src='assets/img/" +
-    //     kittenPics[1] +
-    //     "' alt='cutetwo'></div>" +
-
-    //     "<div class='item'>" +
-    //     "<img class='img-responsive center-block myImage' src='assets/img/" +
-    //     kittenPics[2] +
-    //     "' alt='cutethree'></div>" +
-
-    //     "<div class='item'>" +
-    //     "<img class='img-responsive center-block myImage' src='assets/img/" +
-    //     kittenPics[3] +
-    //     "' alt='cutefour'></div>" +
-
-    //     "<div class='item'>" +
-    //     "<img class='img-responsive center-block myImage' src='assets/img/" +
-    //     kittenPics[4] +
-    //     "' alt='cutefive'></div></div></div>");
-    //     $("#innerCar").carousel();
-
-    //  build an alternative to a carousel
-    // use animate to bring in a picture that changes from a timer
-    //  so 1st, call the timer
-    timer();
+    
+    // use animate to bring in a picture that changes from a timer function called interval
+    //  so 1st, call the interval function
+    interval();
     // the timer function
-    function timer() {
+    function interval() {
         // get a new randome new index
         if ($("button#topicButton").css("font-size") === "14px") {
-            //console.log("In timer(), font-size should be 14: " + $("button#topicButton").css("font-size"));
+            //console.log("In interval(), font-size should be 14: " + $("button#topicButton").css("font-size"));
             kittenIndex = Math.floor(Math.random() * kittenPortPics.length);
         } else {
-            //console.log("In timer(), font-size should be 26: " + $("button#topicButton").css("font-size"));
+            //console.log("In interval(), font-size should be 26: " + $("button#topicButton").css("font-size"));
             kittenIndex = Math.floor(Math.random() * kittenLandPics.length);
         }
             buildBackground();
-            myTimer = setTimeout(function(){ timer() }, 7000);
+            myTimer = setTimeout(function(){ interval() }, 7000);
     }
 
     // build the image function
