be5b3950b5b880c0f4f00f3f53ede60933a85125
Tom McLaughlin <mclaughlintomd@gmail.com>
2021-01-20 20:16:36 -0600

    added chartjs label probs fixed create array

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -46,6 +46,8 @@ var thisSex;
 var thisAge;
 var thisWeight;
 var thisSize;
+var dataPointsArraySize = [];
+var dataPointsArrayWeight = [];
 
 $(document).ready(function(){
   $(document).ready(function(){ feedKittenTimer(); });
@@ -640,7 +642,23 @@ $(document).ready(function(){
   // Only the chosen kitten's data will be displayed on the chart below (above?) the kitten's
   // metric boxes.
   function displayChart() {
-
+    // below, dataPoints is an array of objects. I'm not really joining 2 arrays
+    // so , how do I build that here  {x:sortedAges, y:sortedSizes} and then pushed into an array
+    // sortedAges is one array, sorted Sizes is another
+    
+    for (i=0; i<sortedAges.length; i++) {
+      var resultObjectSize = {};
+      var resultObjectWeight = {};
+      resultObjectSize={ x : sortedAges[i] , y : sortedSizes[i]};
+      resultObjectWeight={ x : sortedAges[i] , y : sortedWeights[i]};
+      console.log("resultObjectSize: ", resultObjectSize);
+      console.log("resultObjectWeight: ", resultObjectWeight);
+      dataPointsArraySize.push(resultObjectSize);
+      dataPointsArrayWeight.push(resultObjectWeight);
+    }
+    console.log("HERE! the new dataPointsArraySize: ", dataPointsArraySize);
+    console.log("HERE! the new dataPointsArrayWeight: ", dataPointsArrayWeight);
+    //
     var chartOptions = {
       animationEnabled: true,
       theme: "light2",
@@ -654,7 +672,7 @@ $(document).ready(function(){
       axisY: {
         title: "Number of Sales",
         suffix: "K",
-        minimum: 30
+        minimum: 0
       },
       toolTip:{
         shared:true
@@ -669,52 +687,20 @@ $(document).ready(function(){
       data: [{
         type: "line",
         showInLegend: true,
-        name: "Projected Sales",
+        name: "Kitten Size",
         markerType: "square",
-        xValueFormatString: "DD MMM, YYYY",
+        //xValueFormatString: "DD MMM, YYYY",
         color: "#F08080",
-        yValueFormatString: "#,##0K",
-        dataPoints: [
-          { x: new Date(2017, 10, 1), y: 63 },
-          { x: new Date(2017, 10, 2), y: 69 },
-          { x: new Date(2017, 10, 3), y: 65 },
-          { x: new Date(2017, 10, 4), y: 70 },
-          { x: new Date(2017, 10, 5), y: 71 },
-          { x: new Date(2017, 10, 6), y: 65 },
-          { x: new Date(2017, 10, 7), y: 73 },
-          { x: new Date(2017, 10, 8), y: 96 },
-          { x: new Date(2017, 10, 9), y: 84 },
-          { x: new Date(2017, 10, 10), y: 85 },
-          { x: new Date(2017, 10, 11), y: 86 },
-          { x: new Date(2017, 10, 12), y: 94 },
-          { x: new Date(2017, 10, 13), y: 97 },
-          { x: new Date(2017, 10, 14), y: 86 },
-          { x: new Date(2017, 10, 15), y: 89 }
-        ]
+        //yValueFormatString: "#,##0K",
+        dataPoints: dataPointsArraySize
       },
       {
         type: "line",
         showInLegend: true,
-        name: "Actual Sales",
+        name: "Kitten Weight",
         lineDashType: "dash",
-        yValueFormatString: "#,##0K",
-        dataPoints: [
-          { x: new Date(2017, 10, 1), y: 60 },
-          { x: new Date(2017, 10, 2), y: 57 },
-          { x: new Date(2017, 10, 3), y: 51 },
-          { x: new Date(2017, 10, 4), y: 56 },
-          { x: new Date(2017, 10, 5), y: 54 },
-          { x: new Date(2017, 10, 6), y: 55 },
-          { x: new Date(2017, 10, 7), y: 54 },
-          { x: new Date(2017, 10, 8), y: 69 },
-          { x: new Date(2017, 10, 9), y: 65 },
-          { x: new Date(2017, 10, 10), y: 66 },
-          { x: new Date(2017, 10, 11), y: 63 },
-          { x: new Date(2017, 10, 12), y: 67 },
-          { x: new Date(2017, 10, 13), y: 66 },
-          { x: new Date(2017, 10, 14), y: 56 },
-          { x: new Date(2017, 10, 15), y: 64 }
-        ]
+        //yValueFormatString: "#,##0K",
+        dataPoints: dataPointsArrayWeight
       }]
     };
     $("#chartContainer").CanvasJSChart(chartOptions);
@@ -727,6 +713,5 @@ $(document).ready(function(){
       }
       e.chart.render();
     }
-    
   }  // end of function displayChart()
 });
