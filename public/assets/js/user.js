e5cf1e565afd9824f2f753c762fad75935bc74e4
Tom McLaughlin <mclaughlintomd@gmail.com>
2021-07-26 19:20:28 -0500

    oops now delete console.logs

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -226,8 +226,8 @@ jQuery(document).ready(function( $ ){
       } else if (currentUserLoggedIn === "true") {
         // I do save the name of the current user in local storage from the
         // login.js page. This is a good check for errors.
-        //console.log(!!!CHECKING: nowCurrentUser[0].name: " + nowCurrentUser[0].name +
-          "   localStorage currentUser: " + currentUser);
+        // console.log(!!!CHECKING: nowCurrentUser[0].name: " + nowCurrentUser[0].name +
+        //   "   localStorage currentUser: " + currentUser);
         //$("span#currentUser").text(nowCurrentUser[0].name);
         // this .forEach goes through each kitten of the user
         // It gets the kitten document and name of kitten, then prints row of buttons
