33ab9a86c2b9c9595fd55eda4cc24b366f125b6f
Tom McLaughlin <mclaughlintomd@gmail.com>
2021-08-18 14:12:43 -0500

    got rid of old unused files

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ var path = require('path');
 var sortAges = require("sort-ids");
 var reorder = require("array-rearrange");
 
-// Require all models
-// MIGHT not need this here - leftover from news scraper app that had everything in server file
-//var imgModel = require('./model'); // imgModel will not be used in future as images
-// is now a part of the kitten model.
-
 // make port ready for deployment on heroku as well as local
 var PORT = process.env.PORT || 3000;
 
@@ -29,56 +24,6 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 //  express.static to serve the public folder as a static directory
 app.use(express.static("public"));
-
-//Step 5
-// var multer = require('multer');
- 
-// var storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads')
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// });
-//var upload = multer({ storage: storage });
-
-// Step 7 - the GET request handler that provides the HTML UI
- 
-// app.get('/', (req, res) => {
-//   imgModel.find({}, (err, items) => {
-//       if (err) {
-//           console.log(err);
-//           res.status(500).send('An error occurred', err);
-//       }
-//       else {
-//           res.render('/user', { items: items });  //I just added the / in front of user (no idea)
-//       }
-//   });
-// });
-
-// Step 8 - the POST handler for processing the uploaded file
- 
-// app.post('/', upload.single('image'), (req, res, next) => {
- 
-//   var obj = {
-//       name: req.body.name,
-//       desc: req.body.desc,
-//       img: {
-//           data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-//           contentType: 'image/png'
-//       }
-//   }
-//   imgModel.create(obj, (err, item) => {
-//       if (err) {
-//           console.log("getting an error!!: ", err);
-//       }
-//       else {
-//           item.save();  // save() is a mongoose function
-//           res.redirect("/user");
-//       }
-//   });
-// });
 
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
@@ -100,10 +45,6 @@ mongoose.Promise = Promise;
     }
    });
   }
-// previous code (from class). Keeping for reference
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/littlecats", { useNewUrlParser: true };
-//mongoose.connect(MONGODB_URI);
-//mongoose.connect('mongodb://localhost:27017/Notification',{ useNewUrlParser: true });
 
 // // Routes
 require("./routes/api-routes.js")(app);
